a28a05875a2416c2c061eaa63c8a68f0ede0d534
Prabhanjan Prabhu <prabhanjanp500@gmail.com>
2024-05-21 14:26:43 +0530

    initial commit

---
+++ components/models/Yolo.tsx
@@ -0,0 +1,278 @@
+import ndarray from "ndarray";
+import { Tensor } from "onnxruntime-web";
+import ops from "ndarray-ops";
+import ObjectDetectionCamera from "../ObjectDetectionCamera";
+import { round } from "lodash";
+import { yoloClasses } from "../../data/yolo_classes";
+import { useState } from "react";
+import { useEffect } from "react";
+import { runModelUtils } from "../../utils";
+
+const RES_TO_MODEL: [number[], string][] = [
+  [[256,256], "yolov7-tiny_256x256.onnx"],
+  [[320, 320], "yolov7-tiny_320x320.onnx"],
+  [[640, 640], "yolov7-tiny_640x640.onnx"],
+  [[256, 256], "best.onnx"],
+];
+
+const Yolo = (props: any) => {
+  const [modelResolution, setModelResolution] = useState<number[]>(
+    RES_TO_MODEL[0][0]
+  );
+  const [modelName, setModelName] = useState<string>(RES_TO_MODEL[0][1]);
+  const [session, setSession] = useState<any>(null);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const session = await runModelUtils.createModelCpu(
+        `./_next/static/chunks/pages/${modelName}`
+      );
+      setSession(session);
+    };
+    getSession();
+  }, [modelName]);
+
+  const changeModelResolution = () => {
+    const index = RES_TO_MODEL.findIndex((item) => item[0] === modelResolution);
+    if (index === RES_TO_MODEL.length - 1) {
+      setModelResolution(RES_TO_MODEL[0][0]);
+      setModelName(RES_TO_MODEL[0][1]);
+    } else {
+      setModelResolution(RES_TO_MODEL[index + 1][0]);
+      setModelName(RES_TO_MODEL[index + 1][1]);
+    }
+  };
+
+  const resizeCanvasCtx = (
+    ctx: CanvasRenderingContext2D,
+    targetWidth: number,
+    targetHeight: number,
+    inPlace = false
+  ) => {
+    let canvas: HTMLCanvasElement;
+
+    if (inPlace) {
+      // Get the canvas element that the context is associated with
+      canvas = ctx.canvas;
+
+      // Set the canvas dimensions to the target width and height
+      canvas.width = targetWidth;
+      canvas.height = targetHeight;
+
+      // Scale the context to the new dimensions
+      ctx.scale(
+        targetWidth / canvas.clientWidth,
+        targetHeight / canvas.clientHeight
+      );
+    } else {
+      // Create a new canvas element with the target dimensions
+      canvas = document.createElement("canvas");
+      canvas.width = targetWidth;
+      canvas.height = targetHeight;
+
+      // Draw the source canvas into the target canvas
+      canvas
+        .getContext("2d")!
+        .drawImage(ctx.canvas, 0, 0, targetWidth, targetHeight);
+
+      // Get a new rendering context for the new canvas
+      ctx = canvas.getContext("2d")!;
+    }
+
+    return ctx;
+  };
+
+  const preprocess = (ctx: CanvasRenderingContext2D) => {
+    const resizedCtx = resizeCanvasCtx(
+      ctx,
+      modelResolution[0],
+      modelResolution[1]
+    );
+
+    const imageData = resizedCtx.getImageData(
+      0,
+      0,
+      modelResolution[0],
+      modelResolution[1]
+    );
+    const { data, width, height } = imageData;
+    // data processing
+    const dataTensor = ndarray(new Float32Array(data), [width, height, 4]);
+    const dataProcessedTensor = ndarray(new Float32Array(width * height * 3), [
+      1,
+      3,
+      width,
+      height,
+    ]);
+
+    ops.assign(
+      dataProcessedTensor.pick(0, 0, null, null),
+      dataTensor.pick(null, null, 0)
+    );
+    ops.assign(
+      dataProcessedTensor.pick(0, 1, null, null),
+      dataTensor.pick(null, null, 1)
+    );
+    ops.assign(
+      dataProcessedTensor.pick(0, 2, null, null),
+      dataTensor.pick(null, null, 2)
+    );
+
+    ops.divseq(dataProcessedTensor, 255);
+
+    const tensor = new Tensor("float32", new Float32Array(width * height * 3), [
+      1,
+      3,
+      width,
+      height,
+    ]);
+
+    (tensor.data as Float32Array).set(dataProcessedTensor.data);
+    return tensor;
+  };
+
+  const conf2color = (conf: number) => {
+    const r = Math.round(255 * (1 - conf));
+    const g = Math.round(255 * conf);
+    return `rgb(${r},${g},0)`;
+  };
+
+  const postprocess = async (
+    tensor: Tensor,
+    inferenceTime: number,
+    ctx: CanvasRenderingContext2D
+  ) => {
+    const dx = ctx.canvas.width / modelResolution[0];
+    const dy = ctx.canvas.height / modelResolution[1];
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    for (let i = 0; i < tensor.dims[0]; i++) {
+      //let [batch_id, x0, y0, x1, y1, cls_id, score] = tensor.data.slice(
+      //  i * 7,
+      //  i * 7 + 7
+      //);
+
+      let output = tensor.data;
+      //console.log(output);
+      const proc_output = [];
+    
+      // tensor is 1 x 83 x 1344
+      //console.log(proc_output);
+      let [x0, y0, x1, y1] = [0, 0, 0, 0];
+      let [a, b, c, d] = [0, 0, 0, 0];
+
+      //const all_bbox_coords = output.slice(0, 4 * 1344);
+      ctx.strokeStyle = "green";
+      ctx.lineWidth = 3;
+      //ctx.strokeRect(a, b, c, d);
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "green";
+      let max_sure_index = -1;
+      let max_sure_conf = 0;
+      for (let i = 0; i < 1344; i += 1) {
+        x0 = output[i];
+        x1 = output[1344 + i];
+        y0 = output[2688 + i];
+        y1 = output[4032 + i];
+        [a, b, c, d] = [x0 - x1 / 2, y0 - y1 / 2, x1, y1].map((x: any) => round(x));
+
+        let cur_conf = 0;
+        let flag = 1;
+        cur_conf = output[5376 + i];
+        if (cur_conf > max_sure_conf) {
+          max_sure_conf = cur_conf;
+          max_sure_index = i;
+        }
+        // for (let j = 7; j < 83; j += 3) {
+        //   cur_conf += output[j * 1344 + i];
+        //   if (output[(j-2) * 1344 + i] < x0 - x1/2 || output[(j-2) * 1344 + i] > (x0 + x1/2)) flag = 0;
+        //   if (output[(j-1) * 1344 + i] < y0 - y1/2 || output[(j-1) * 1344 + i] > (y0 + y1/2)) flag = 0;
+        // }
+
+
+        // if (cur_conf > max_sure_conf && output[5376 + i] > 0.001 && flag == 1) {
+        //   max_sure_conf = cur_conf;
+        //   max_sure_index = i;
+        // }
+      }
+      console.log(max_sure_conf, max_sure_index);
+      x0 = output[max_sure_index] * dx;
+      x1 = output[1344 + max_sure_index] * dx;
+      y0 = output[2688 + max_sure_index] * dy;
+      y1 = output[4032 + max_sure_index] * dy;
+      [a, b, c, d] = [x0 - x1 / 2, y0 - y1 / 2, x1, y1].map((x: any) => round(x));
+      //ctx.strokeRect(a, b, c, d);
+      console.log(a, b, c, d);
+
+      
+      for (let i = 0; i < tensor.dims[1]; i++) {
+        //console.log(tensor.dims[2] * i + tensor.dims[2] - 1)
+        proc_output.push(output[tensor.dims[2] * i + max_sure_index]);
+      }
+      console.log(proc_output);
+
+      
+
+
+
+      // scale to canvas size
+      //[x0, x1] = [x0, x1].map((x: any) => x * dx);
+      //[y0, y1] = [y0, y1].map((x: any) => x * dy);
+      //[x0, x1, y0, y1] = [x0, x1, y0, y1].map((x: any) => round(x));
+      //[a, b, c, d] = [x0 - x1 / 2, y0 - y1 / 2, x1, y1].map((x: any) => round(x));
+      // [batch_id, x0, y0, x1, y1, cls_id] = [
+      //   batch_id,
+      //   x0,
+      //   y0,
+      //   x1,
+      //   y1,
+      //   cls_id,
+      // ].map((x: any) => round(x));
+
+      // [score] = [score].map((x: any) => round(x * 100, 1));
+      // const label =
+      //   yoloClasses[cls_id].toString()[0].toUpperCase() +
+      //   yoloClasses[cls_id].toString().substring(1) +
+      //   " " +
+      //   score.toString() +
+      //   "%";
+      // const color = conf2color(score / 100);
+
+
+      //console.log([a, b, c, d]);
+      ctx.strokeStyle = "green";
+      ctx.lineWidth = 3;
+      //ctx.strokeRect(a, b, c, d);
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "green";
+      //ctx.fillText(label, x0, y0 - 5);
+
+      // fillrect with transparent color
+      //ctx.fillStyle = color.replace(")", ", 0.2)").replace("rgb", "rgba");
+      //ctx.fillRect(x0, y0, x1 - x0, y1 - y0);
+        ctx.fillStyle = "red";
+
+      for (let i = 5; i < tensor.dims[1]; i+=3) {
+        //console.log(proc_output[i], proc_output[i+1]);
+        if (proc_output[i + 2] > 0.6) ctx.fillRect(round(proc_output[i] * dx), round(proc_output[i+1] * dy), 5, 5);
+      }
+      // size is 635 * 475 in normal case
+      //ctx.fillRect(635, 475, 10, 10);
+    }
+  };
+
+  return (
+    <ObjectDetectionCamera
+      width={props.width}
+      height={props.height}
+      preprocess={preprocess}
+      postprocess={postprocess}
+      resizeCanvasCtx={resizeCanvasCtx}
+      session={session}
+      changeModelResolution={changeModelResolution}
+      modelName={modelName}
+    />
+  );
+};
+
+export default Yolo;
